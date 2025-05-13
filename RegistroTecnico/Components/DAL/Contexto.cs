da996a4b745c2f87febf7068cfe905833648d00e
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-05-13 14:40:42 -0600

    Crea el contexto

---
+++ RegistroTecnico/Components/DAL/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnico.Components.Models;
+
+namespace RegistroTecnico.Components.DAL
+{
+    public class Contexto:DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options):base (options){ }   
+
+        public DbSet<Tecnicos> Tecnicos {  get; set; }  
+    }
+    
+}
