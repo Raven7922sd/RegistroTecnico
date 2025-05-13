197746851d578a706ec06f3786ade073f9fe18e9
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-05-13 12:14:33 -0600

    Crea la clase modelo Tecnicos con sus atributos

---
+++ RegistroTecnico/Components/Models/Tecnicos.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnico.Components.Models
+{
+    public class Tecnicos
+    {
+        [Key]
+        public int TecnicoId { get; set; }
+
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string TecnicoNombre { get; set; } = null!;
+
+        [Range(0.1, double.MaxValue, ErrorMessage = "Este campo es obligatorio y debe ser mayor que 0")]
+        public double SueldoHora { get; set; }
+            
+      
+
+
+    }
+}
