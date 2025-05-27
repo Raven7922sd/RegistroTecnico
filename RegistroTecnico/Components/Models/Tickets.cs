d8a29cd233217b221620194b0eaecfe4a9709c93
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-05-27 13:57:06 -0600

    Clase tickets completada

---
+++ RegistroTecnico/Components/Models/Tickets.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnico.Components.Models;
+public class Tickets
+{
+    [Key]
+    public int TicketId { get; set; }
+    public DateOnly Fecha { get; set; }= DateOnly.FromDateTime(DateTime.Now);
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    public double TiempoInvertido { get;set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    public string Prioridad { get; set; }
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    public string Asunto { get; set; }
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    public string Descripcion{ get; set; }
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    public int TecnicoId { get; set; }
+    public Clientes cliente {  get; set; }
+    public Tecnicos tecnico { get; set; }
+}
