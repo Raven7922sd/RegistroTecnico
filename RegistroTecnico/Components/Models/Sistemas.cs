0cbf7960fbb907c03e970cdbc2cd40436233741b
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-06-12 17:02:21 -0600

    Modelo Sistemas completado

---
+++ RegistroTecnico/Components/Models/Sistemas.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnico.Components.Models;
+
+public class Sistemas
+{
+    [Key]
+    public int SistemaId { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    [StringLength(50, ErrorMessage = "Máximo 500 caracteres por descripción")]
+    public string Descripcion { get; set; } = null!;
+
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    [Range(0,100,ErrorMessage ="El rango es de '0-100'")]
+    public int Complejidad { get; set; }
+}
