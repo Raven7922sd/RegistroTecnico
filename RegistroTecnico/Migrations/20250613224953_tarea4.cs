f5e1f70085a7a6a8eb4a93b4f70e4690808eab44
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-06-13 23:08:16 -0600

    Migración creada

---
+++ RegistroTecnico/Migrations/20250613224953_tarea4.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroTecnico.Migrations
+{
+    /// <inheritdoc />
+    public partial class tarea4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Tickets",
+                type: "nvarchar(220)",
+                maxLength: 220,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Asunto",
+                table: "Tickets",
+                type: "nvarchar(220)",
+                maxLength: 220,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "Sistemas",
+                columns: table => new
+                {
+                    SistemaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Complejidad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fecha = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sistemas", x => x.SistemaId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sistemas");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Tickets",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(220)",
+                oldMaxLength: 220);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Asunto",
+                table: "Tickets",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(220)",
+                oldMaxLength: 220);
+        }
+    }
+}
