cfb14d9e7430109b9dd325f2263d4566c6f5faed
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-06-16 09:49:10 -0600

    Componente Index mejorado

--- RegistroTecnico/Components/Services/SistemaService.cs
+++ RegistroTecnico/Components/Services/SistemaService.cs
@@ -18,7 +18,7 @@ public class SistemaService(IDbContextFactory<Contexto> DbFactory)
         }
         else { return await ModificarSistema(sistema); }
     }
-
+    
     public async Task<bool> ExisteSistema(int SistemaId)
     {
         await using var context = await DbFactory.CreateDbContextAsync();
@@ -57,22 +57,28 @@ public class SistemaService(IDbContextFactory<Contexto> DbFactory)
         return await context.Sistemas.FirstOrDefaultAsync(s => s.SistemaId==SistemaId);
     }
 
-    public async Task<PaginacionResultado<Sistemas>> BuscarSistemasAsync(
-      string filtroCampo,
-      string valorFiltro,
-      DateTime? fechaDesde,
-      DateTime? fechaHasta,
-      int pagina,
-      int tamanioPagina)
+   public async Task<PaginacionResultado<Sistemas>> BuscarSistemasAsync(
+   string filtroCampo,
+   string valorFiltro,
+   DateTime? fechaDesde,
+   DateTime? fechaHasta,
+   int pagina,
+   int tamanioPagina)
     {
         Expression<Func<Sistemas, bool>> filtro = s => true;
 
-        if (filtroCampo == "SistemaId" && int.TryParse(valorFiltro, out var sistemaId))
+        if (filtroCampo == "SistemaId" && !string.IsNullOrWhiteSpace(valorFiltro) && int.TryParse(valorFiltro, out var sistemaId))
+        {
             filtro = filtro.AndAlso(s => s.SistemaId == sistemaId);
-        else if (filtroCampo == "Descripcion")
+        }
+        else if (filtroCampo == "Descripcion" && !string.IsNullOrWhiteSpace(valorFiltro))
+        {
             filtro = filtro.AndAlso(s => s.Descripcion.ToLower().Contains(valorFiltro.ToLower()));
-        else if (!string.IsNullOrEmpty(filtroCampo))
+        }
+        else if (filtroCampo is "Baja" or "Media" or "Alta")
+        {
             filtro = filtro.AndAlso(t => t.Complejidad.ToLower() == filtroCampo.ToLower());
+        }
 
         if (fechaDesde.HasValue)
             filtro = filtro.AndAlso(s => s.Fecha >= DateOnly.FromDateTime(fechaDesde.Value));
