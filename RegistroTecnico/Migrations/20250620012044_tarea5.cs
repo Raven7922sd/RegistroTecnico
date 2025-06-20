bc931e42badda5ef19574523b24ae45ff23887dd
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-06-20 09:44:28 -0600

    Migración tarea5 creada

---
+++ RegistroTecnico/Migrations/20250620012044_tarea5.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroTecnico.Migrations
+{
+    /// <inheritdoc />
+    public partial class tarea5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Sistemas",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+         
+            migrationBuilder.AddColumn<int>(
+                name: "Existencia",
+                table: "Sistemas",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VentasDetalles");
+
+            migrationBuilder.DropTable(
+                name: "Ventas");
+
+            migrationBuilder.DropColumn(
+                name: "Coste",
+                table: "Sistemas");
+
+            migrationBuilder.DropColumn(
+                name: "Existencia",
+                table: "Sistemas");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Sistemas",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+        }
+    }
+}
