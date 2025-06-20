71e605738cf1222ed97b479ceb7c8c2bc8ab8166
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-06-20 09:45:25 -0600

    Migración eliminarMonto creada

---
+++ RegistroTecnico/Migrations/20250620053533_EliminarMontoDeVentas.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroTecnico.Migrations
+{
+    /// <inheritdoc />
+    public partial class EliminarMontoDeVentas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Monto",
+                table: "Ventas");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Monto",
+                table: "Ventas",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
