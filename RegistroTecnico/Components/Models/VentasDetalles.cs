47a7f29731a12d21e96a6935c5c6582daa601531
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-06-20 09:40:10 -0600

    Ventas Detalles completada

---
+++ RegistroTecnico/Components/Models/VentasDetalles.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnico.Components.Models;
+
+public class VentasDetalles
+{
+    [Key]
+    public int DetalleId { get; set; }
+
+    public double Monto { get; set; }
+
+    public int Cantidad { get; set; }
+
+    public double ValorCobrado { get; set; }
+
+    public int SistemaId { get; set; }
+    [ForeignKey("SistemaId")]
+    public virtual Sistemas Sistema { get; set; } = null!;
+
+    public int VentaId { get; set; }
+    [ForeignKey("VentaId")]
+    [InverseProperty("VentasDetalles")]
+    public virtual Ventas Venta { get; set; } = null!;
+}
