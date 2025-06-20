d8a205473a71a83741d8f5e622496f99ac63fc26
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-06-20 09:34:07 -0600

    Ventas creada

---
+++ RegistroTecnico/Components/Models/Ventas.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnico.Components.Models;
+public class Ventas
+{
+    [Key]
+    public int VentaId { get; set; }
+
+    public DateOnly Fecha { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+
+    [Required(ErrorMessage = "Debe seleccionar un cliente")]
+    [Range(1, int.MaxValue, ErrorMessage = "Debe seleccionar un cliente")]
+    public int ClienteId { get; set; }
+   
+    public Clientes Cliente { get; set; } = null!;
+
+    [InverseProperty("Venta")]
+    public virtual ICollection<VentasDetalles> VentasDetalles { get; set; } = new List<VentasDetalles>();
+}
